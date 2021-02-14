c98da21381e1922425d0755b5bc791b32cc640cf
KononOleg <kononoleg97@gmail.com>
2021-02-14 19:36:26 +0300

    feat: add solutions Carbon-dating, Dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(array) {
+
+  if(!array)
+  return false;
+
+  var res='';
+  for(var i=0;i<array.length;i++)
+  {
+    if(typeof array[i]==="string")
+    {
+      array[i]=array[i].trim();
+        res+=array[i][0];
+    }
+  }
+  res=res.toUpperCase();
+  return res.split('').sort().join('');
 };
