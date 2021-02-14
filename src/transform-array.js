7d35f231d1caca4b54bcb8e463e1bc43978992e9
KononOleg <kononoleg97@gmail.com>
2021-02-14 13:20:43 +0300

    feat: add solutions - What-season, Counts-cats, Transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+  if (!Array.isArray(arr)) 
+  throw new Error();
+ 
+  var resArray=[];
+  
+  for(var i=0;i<arr.length;i++)
+  {
+    if(arr[i]=="--discard-next")
+    {
+        i++;
+    }
+    else if(arr[i]=="--discard-prev")
+    {
+      resArray.splice(i-1,1);
+    }
+    else if(arr[i]=="--double-next")
+    {
+      resArray.push(arr[i]);
+    }
+    else if(arr[i]=="--double-prev")
+    {
+      resArray.push(arr[i-1]);
+    }
+    else 
+    {
+      resArray.push(arr[i]);
+    }
+  }
+
+  return resArray;
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if(!date) 
+    return 'Unable to determine the time of year!';
+
+   if (typeof date === 'string') 
+      throw new Error();
+
+  if (!date.getTime()) 
+      throw new Error();
+      
+  var season="";
+  if(date.getMonth()>4 && date.getMonth()<8)
+  {
+    season="summer";
+  }
+  else if(date.getMonth()>7 && date.getMonth()<11)
+  {
+    season="autumn";
+  }
+  else if(date.getMonth()>10 || date.getMonth()<2)
+  {
+    season="winter";
+  }
+  else if(date.getMonth()>1 && date.getMonth()<5)
+  {
+    season="spring";
+  }
+    
+    return season;
 };
